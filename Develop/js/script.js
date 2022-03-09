81ef05ddf9155bc6ab780c14446ee718d212d152
Rajveer Singh <rajsidhu2924@gmail.com>
2022-03-08 22:34:13 -0800

    questions updated in index.js

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -19,55 +19,55 @@ let option2 = document.querySelector('.option2');
 let option3 = document.querySelector('.option3');
 let option4 = document.querySelector('.option4');
 
-// questions for MCQ. using the same question for testing purposes. questions will be updated later.
+// questions for MCQ.
 let questions =
     [{
-        question: "Who invented JavaScript?",
-        a: "Douglas Crockford",
-        b: "Sheryl Sandberg",
-        c: "Brendan Eich",
-        d: "Brendan Eich",
+        question: "Which type of JavaScript language is ___",
+        a: "Object-Oriented",
+        b: "Object-Based",
+        c: "Assembly-language",
+        d: "High-level",
+        rightAnswer: "b"
+    },
+    {
+        question: "Which one of the following also known as Conditional Expression:",
+        a: "Alternative to if-else",
+        b: "Switch statement",
+        c: "If-then-else statement",
+        d: "immediate if",
+        rightAnswer: "d"
+    },
+    {
+        question: "In JavaScript, what is a block of statement?",
+        a: "Conditional block",
+        b: "block that combines a number of statements into a single compound statement",
+        c: "both conditional block and a single statement",
+        d: "block that contains a single statement",
+        rightAnswer: "b"
+    },
+    {
+        question: "When interpreter encounters an empty statements, what it will do:",
+        a: "Shows a warning",
+        b: "Prompts to complete the statement",
+        c: "Throws an error",
+        d: "Ignores the statements",
+        rightAnswer: "d"
+    },
+    {
+        question: "The 'function' and  'var' are known as:",
+        a: "Keywords",
+        b: "Data types",
+        c: "Declaration statements",
+        d: "Prototypes",
         rightAnswer: "c"
     },
     {
-        question: "Who invented JavaScript?",
-        a: "Douglas Crockford",
-        b: "Sheryl Sandberg",
-        c: "Brendan Eich",
-        d: "Brendan Eich",
-        rightAnswer: "c"
-    },
-    {
-        question: "Who invented Java?",
-        a: "Douglas Crockford",
-        b: "Sheryl Sandberg",
-        c: "Brendan Eich",
-        d: "Brendan Eich",
-        rightAnswer: "c"
-    },
-    {
-        question: "Who invented React?",
-        a: "Douglas Crockford",
-        b: "Sheryl Sandberg",
-        c: "Brendan Eich",
-        d: "Brendan Eich",
-        rightAnswer: "c"
-    },
-    {
-        question: "Which is a JavaScript package manager?",
-        a: "Node.js",
-        b: "TypeScript",
-        c: "npm",
-        d: "Brendan Eich",
-        rightAnswer: "c"
-    },
-    {
-        question: "Who invented C##?",
-        a: "Douglas Crockford",
-        b: "Sheryl Sandberg",
-        c: "Brendan Eich",
-        d: "Brendan Eich",
-        rightAnswer: "c"
+        question: "Which one of the following is the correct way for calling the JavaScript code?",
+        a: "Preprocessor",
+        b: "Triggering Event",
+        c: "RMI",
+        d: "Function/Method",
+        rightAnswer: "d"
     }
     ];
 
